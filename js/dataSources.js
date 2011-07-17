41800b1d150dec679cca4a7af747f375938a9e71
Adam Freidin <adam.freidin@gmail.com>
2011-07-17 13:06:43 -0700

    fix order of initial datasource emit

--- js/dataSources.js
+++ js/dataSources.js
@@ -2,7 +2,7 @@ var githubDS = new DataSource({
   description: "Mashboard GitHub commits",
   url: "https://github.com/api/v2/json/commits/list/mikhailxu/Mashboard/master", 
   process: function(data) {
-    for(var i=0; i<data.commits.length; i++) {
+    for(var i=data.commits.length-1; i>=0; i--){
       this.emit(data.commits[i]);
     }
   }
@@ -14,7 +14,7 @@ var twitterDS = new DataSource({
   description: "#iosdevcamp Tweets",
   url: "http://search.twitter.com/search.json?q=%23iosdevcamp&rpp=5",
   process: function(data){
-    for(var i=0; i<data.results.length; i++) {
+    for(var i=data.results.length-1; i>=0; i--){
       this.emit(data.results[i]);
     }
   }
